ea458b9d4fa7e4221424b5fb185dda5c6e4c8c10
Leonardo Takehana <leonardotakehana@gmail.com>
2025-05-04 21:11:51 -0300

    Correção de lógica do front-end

--- web/src/Services/GatewayServices.js
+++ web/src/Services/GatewayServices.js
@@ -2,12 +2,12 @@ import services from "./sevices";
 
 export class GatewayServices {
     encriptWords = async (plaintext, key) => {
-        const response = await services.post('/encrypt/', {plaintext: plaintext, key: key})
-        return response.data
+        const response = await services.post('/encrypt/', {text: plaintext, key: key})
+        return response.data.result
     }
 
     decriptWords = async (ciphertext, key) => {
-        const response = await services.post('/decrypt/', {ciphertext: ciphertext, key: key})
-        return response.data
+        const response = await services.post('/decrypt/', {text: ciphertext, key: key})
+        return response.data.result
     }
 }
--- web/src/Services/sevices.js
+++ web/src/Services/sevices.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: import.meta.API_GATEWAY_ADDRESS,
-})
+    baseURL: 'http://192.168.100.101:8080',
+});
 
 export default api;
